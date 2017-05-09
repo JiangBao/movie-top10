a9e67e393acd6c50c7c7f63ed85d521414e0f9fc
JiangBao <873575504@qq.com>
2017-05-09 16:10:52 +0800

    add client code

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>豆瓣电影TOP10</h1>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+h1 {
+  text-align: center
+}
+</style>
---
+++ client/src/components/Movie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="movie">
+
+    </div>
+</template>
+
+<script>
+    name: 'movie',
+    data () {
+        return {
+            data: null
+        }
+    }
+</script>
+ 
