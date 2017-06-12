ad3bda76b70dc8bc83671c0656ce9f25c325f7ea
JiangBao <873575504@qq.com>
2017-06-12 16:23:35 +0800

    complete

--- server/app.js
+++ server/app.js
@@ -3,6 +3,16 @@ var movieData = require('./routes/movieData.js');
 
 var app = express();
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 // '/movie-data'路径提供top10电影数据
 app.use('/movie-data', movieData);
 
